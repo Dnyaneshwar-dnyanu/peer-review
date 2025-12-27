631c882f3ad1406b141ce11e8ec3aab95e26ee4a
Dnyaneshwar Bhajantri <dnyaneshwarbhajantri18@gmail.com>
2025-12-27 15:23:43 +0530

    update the database url

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,11 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 let connectDB = async () => {
      try {
+          console.log("Trying to connect to DB");
           await mongoose.connect(process.env.MONGO_URL);
+          console.log("Mongodb connected");
+          
      }
      catch (err) {
           console.error(err);
